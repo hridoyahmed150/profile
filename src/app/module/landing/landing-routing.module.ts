4cf81d9ee6a61df2a485a472aac08ea07218cea5
Hridoy Ahmed <hridoyahmed150@gmail.com>
2022-10-14 23:00:05 +0600

    work add

--- src/app/module/landing/landing-routing.module.ts
+++ src/app/module/landing/landing-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule, ViewChild } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectViewComponent } from '../project/project-view/project-view.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 const routes: Routes = [
   {
     path: '',
     component: LandingPageComponent
   },
+  {
+    path: 'works',
+    component: ProjectViewComponent
+  }
 ];
 
 @NgModule({
